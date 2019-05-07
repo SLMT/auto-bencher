7c75135d6702b07e2190d879ab7d9d1aeaaa2e70
SLMT <sam123456777@gmail.com>
2019-05-07 20:48:38 +0800

    Fix the bug of scp

--- src/command.rs
+++ src/command.rs
@@ -34,21 +34,35 @@ pub fn ssh(user_name: &str, ip: &str, cmd: &str) -> Result<String, BenchError> {
     Ok(output)
 }
 
-pub fn scp(is_dir: bool, user_name: &str, ip: &str, local_path: &str, remote_path: &str) -> Result<(), BenchError> {
+pub fn scp(is_dir: bool, user_name: &str, ip: &str, local_path: &str, remote_path: &str) -> Result<String, BenchError> {
     let mut command = Command::new("scp");
 
     if is_dir {
         command.arg("-r");
     }
-
-    command.arg(format!("{}@{}:{}", user_name, ip, local_path));
-    command.arg(remote_path);
+    
+    command.arg(local_path);
+    command.arg(format!("{}@{}:{}", user_name, ip, remote_path));
 
     trace!("{:?}", command);
 
-    command.output().map_err(|e| BenchError::throw("execute command fails", e))?;
+    let output = command.output().map_err(|e| BenchError::throw("execute command fails", e))?;
 
-    Ok(())
+    match output.status.code() {
+        Some(0) => {
+            Ok(String::from_utf8(output.stdout)
+                    .map_err(|e| BenchError::throw("parsing command output fails", e))?)
+        },
+        Some(2) => {
+            Err(BenchError::FileNotFound)
+        },
+        Some(code) => {
+            Err(BenchError::CommandFails(code))
+        },
+        None => {
+            Err(BenchError::message("the command terminates by a signal"))
+        } 
+    }
 }
 
 pub fn ls(path: &str) -> Result<String, BenchError> {
