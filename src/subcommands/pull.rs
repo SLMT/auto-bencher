78c58d4aa6894fab2577199b1a772d80f90d51ec
SLMT <sam123456777@gmail.com>
2019-09-24 22:21:23 +0800

    Add an option to ignore errors when pulling

--- src/subcommands/pull.rs
+++ src/subcommands/pull.rs
@@ -18,12 +18,17 @@ pub fn get_sub_command<'a, 'b>() -> App<'a, 'b> {
                     .long("separate")
                     .short("s")
                     .help("Separates the pulled files according to their source ip"))
+                .arg(Arg::with_name("IGNORE ERROR")
+                    .long("ignore-error")
+                    .short("i")
+                    .help("If there is an error happens in a job, do not stop and proceed to the next job."))
                 .about("pulls the files whose file name matching the given pattern")
 }
 
 pub fn execute(config: &Config, args: &ArgMatches) -> Result<()> {
     let pattern = args.value_of("PATTERN").unwrap();
     let is_separated = args.is_present("SEPARATE");
+    let ignore_error = args.is_present("IGNORE ERROR");
     let remote_path = format!("{}/{}", &config.system.remote_work_dir, pattern);
 
     let local_dir = "pulls";
@@ -31,12 +36,20 @@ pub fn execute(config: &Config, args: &ArgMatches) -> Result<()> {
 
     for ip in &config.machines.all {
         info!("Pulling files from {}...", &ip);
-       if is_separated {
+        let result = if is_separated {
             let local_path = format!("{}/{}", local_dir, ip);
             fs::create_dir_all(&local_path)?;
-            command::scp_from(false, &config.system.user_name, &ip, &remote_path, &local_path)?;
+            command::scp_from(false, &config.system.user_name, &ip, &remote_path, &local_path)
         } else {
-            command::scp_from(false, &config.system.user_name, &ip, &remote_path, &local_dir)?;
+            command::scp_from(false, &config.system.user_name, &ip, &remote_path, &local_dir)
+        };
+
+        if let Err(e) = result {
+            if ignore_error {
+                error!("{}", e);
+            } else {
+                return Err(e);
+            }
         }
     }
 
