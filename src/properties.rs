320c396ac77197368ac101fa4364c85611cea01e
SLMT <sam123456777@gmail.com>
2019-09-24 15:23:26 +0800

    Add a warning when it cannot find a property or file

--- src/properties.rs
+++ src/properties.rs
@@ -5,6 +5,7 @@ use std::fs::{self, File};
 use std::io::BufReader;
 
 use serde::Deserialize;
+use log::*;
 
 use crate::error::Result;
 
@@ -31,6 +32,8 @@ impl PropertiesFile {
     fn set(&mut self, property: &str, value: &str) {
         if let Some(val) = self.properties.get_mut(property) {
             *val = value.to_owned();
+        } else {
+            warn!("Cannot find property: {} in {}.properties", property, self.filename);
         }
     }
 
@@ -79,6 +82,8 @@ impl PropertiesFileMap {
     pub fn set(&mut self, filename: &str, property: &str, value: &str) {
         if let Some(file) = self.files.get_mut(filename) {
             file.set(property, value);
+        } else {
+            warn!("Cannot find properties file: {}.properties", filename);
         }
     }
 
