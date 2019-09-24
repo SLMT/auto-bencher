9e83b681ad13bf921be7ac603b21a92c53e4e459
SLMT <sam123456777@gmail.com>
2019-09-24 21:37:10 +0800

    Add an option for pulling

--- src/subcommands/pull.rs
+++ src/subcommands/pull.rs
@@ -14,19 +14,30 @@ pub fn get_sub_command<'a, 'b>() -> App<'a, 'b> {
                     .help("The pattern of file names of files that you wish to pull")
                     .required(true)
                     .index(1))
+                .arg(Arg::with_name("SEPARATE")
+                    .long("separate")
+                    .short("s")
+                    .help("Separates the pulled files according to their source ip"))
                 .about("pulls the files whose file name matching the given pattern")
 }
 
 pub fn execute(config: &Config, args: &ArgMatches) -> Result<()> {
     let pattern = args.value_of("PATTERN").unwrap();
+    let is_separated = args.is_present("SEPARATE");
     let remote_path = format!("{}/{}", &config.system.remote_work_dir, pattern);
+
     let local_dir = "pulls";
+    fs::create_dir_all(&local_dir)?;
 
     for ip in &config.machines.all {
         info!("Pulling files from {}...", &ip);
-        let local_path = format!("{}/{}", local_dir, ip);
-        fs::create_dir_all(&local_path)?;
-        command::scp_from(false, &config.system.user_name, &ip, &remote_path, &local_path)?;
+       if is_separated {
+            let local_path = format!("{}/{}", local_dir, ip);
+            fs::create_dir_all(&local_path)?;
+            command::scp_from(false, &config.system.user_name, &ip, &remote_path, &local_path)?;
+        } else {
+            command::scp_from(false, &config.system.user_name, &ip, &remote_path, &local_dir)?;
+        }
     }
 
     Ok(())
